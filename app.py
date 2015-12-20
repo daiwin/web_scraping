039803545465239e8f392b1e39cf3be8aa3d981c
daiwin <Daiwin@bitbucket.org>
2015-12-20 22:07:50 +0500

    Add write file and url gen

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from BeautifulSoup import BeautifulSoup
 import urllib3
 
-
-
-def getHeaders():
+def getHeaders(url):
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://www.mk.ru/culture/2015/12/18/')
+    r = http.request('GET', url)
     soup = BeautifulSoup(''.join(r.data))
     # search header
     a = soup.findAll("a", { "class" : "mkh2" })
@@ -15,27 +13,44 @@ def getHeaders():
         [comment.extract() for comment in comments]
     return a
 
-
-
-def getText():
+def getText(url):
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://www.mk.ru/culture/2015/12/18/')
+    r = http.request('GET', url)
     soup = BeautifulSoup(''.join(r.data))
     # search header
     a = soup.findAll("p", { "class" : "text" })
     return a
-
 
 def concatMass(a,b):
     c = zip(a, b)
     return c
 
 
-b=getText()
-a=getHeaders()
-c=concatMass(a,b)
-print c
+def makeUrl(year,month,day):
+    date = year+'/'+month+'/'+day
+    link = 'http://www.mk.ru/culture/'+date+'/'
+    return link
 
 
 
+year = ['2011']
+month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+day = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19']
+
+
+
+
+for y in year:
+    for m in month:
+        for d in day:
+            url=makeUrl(y, m, d)
+            b=getText(url)
+            if not b:
+                break
+
+            f = open('data/'+y+'_'+m+'_'+d+'.txt', 'w')
+            a=getHeaders(url)
+            c=concatMass(a,b)
+            f.write(str(c))
+            f.close()
 
