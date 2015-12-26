3e32d148c45df5c3e0a668b3f29c1152176c153d
daiwin <Daiwin@bitbucket.org>
2015-12-26 19:30:01 +0500

    Finaly fix2

--- app.py
+++ app.py
@@ -1,63 +1,68 @@
 # -*- coding: utf-8 -*-
 from BeautifulSoup import BeautifulSoup
 import urllib3
+import time # Старый низкоуровневый модуль
+import datetime # Новый модуль для работы с датой и временем
+
 
 def makeUrl(year,month,day):
     date = year+'/'+month+'/'+day+'/'
     link = 'http://www.mk.ru/politics/'+date
     return link
 
-def getHeaders(url):
+def getArticleUrl(url):
     http = urllib3.PoolManager()
     r = http.request('GET', url)
     soup = BeautifulSoup(''.join(r.data))
-    bitch = soup.find("ul", { "class" : "big_listing" })
-    a = bitch.findAll("a", { "class" : "mkh2" })
-
-    list=[]
-
+    some = soup.find("ul", { "class" : "big_listing" })
+    a = some.findAll("a", { "class" : "mkh2" })
+    urlList=[]
     for item in a:
-        comments = item.findAll("span")
-        [comment.extract() for comment in comments]
-        list.append(item.text)
-    return list
+        urlList.append(item["href"])
+    return urlList
 
 
 
-def getText(url):
+def getText(urlList):
     http = urllib3.PoolManager()
-    r = http.request('GET', url)
-    soup = BeautifulSoup(''.join(r.data))
-    # search header
-    a = soup.findAll("p", { "class" : "text" })
+    cleanArts=""
+    for url in urlList:
+        r = http.request('GET', url)
+        soup = BeautifulSoup(''.join(r.data))
+        article = soup.find("div", { "class" : "content" })
 
-    list=[]
-    for item in a:
-        list.append(item.text)
-    return list
+        for p in article.findAll('p'):
+            div = p.findAll("div")
+            [comment.replaceWith(" ") for comment in div]
+            a = p.findAll("a")
+            [comment.replaceWith(" ") for comment in a]
+            span = p.findAll("span")
+            [comment.replaceWith(" ") for comment in span]
+            cleanArts = cleanArts + " " + p.text
+        cleanArts=cleanArts+"\n\n"
+
+    return cleanArts
 
 
-def concatMass(a,b):
-    string = ""
-    for item1, item2 in zip(a, b):
-        string =string + " " + item1+" "+item2
-    return string
 
 
-year = ['2011', '2012', '2013', '2014', '2015']
-month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-day = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15',
-       '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 
-for y in year:
-    for m in month:
-        for d in day:
-            url = makeUrl(y, m, d)
-            b = getText(url)
-            if not b:
-                break
-            f = open('data/'+y+'_'+m+'_'+d+'.txt', 'w')
-            a = getHeaders(url)
-            c = concatMass(a, b)
-            f.write(c.encode('utf8'))
-            f.close()
+
+
+
+
+
+dt = datetime.datetime.now()
+timedelta1 = datetime.timedelta(days=1)
+howMuchDaysInterval = 1
+
+for i in range(howMuchDaysInterval):
+    url = makeUrl(str(dt.year), str(dt.month), str(dt.day))
+    urlList = getArticleUrl(url)
+    if not urlList:
+        break
+    text = getText(urlList)
+    f = open('data/'+str(dt.year)+'_'+str(dt.month)+'_'+str(dt.day)+'.txt', 'w')
+    f.write(text.encode('utf8'))
+    f.close()
+    dt = dt - timedelta1
--- cleaning.py
+++ cleaning.py
@@ -1,56 +1,67 @@
 # -*- coding: utf-8 -*-
+from os import listdir
+from os.path import isfile, join
 import nltk
-import string
 import pymorphy2
-import xlwt
 import re
 from collections import Counter
-
-
 from nltk.corpus import stopwords
-nltk.download('punkt')
 nltk.download('stopwords')
 
+def countArtOfDay(y, m, d):
+    try:
+        f = open("data/" + y + "_" + m + "_"+d+".txt", 'rw')
+    except:
+        raise
+
+    morph = pymorphy2.MorphAnalyzer()
+    articleText = ""
+    stop_words = stopwords.words('russian')
+    stop_words.extend([u'это', u'как', u'так', u'и', u'в', u'над', u'к', u'до', u'не', u'на', u'но', u'за', u'то', u'с', u'ли',
+    u'а', u'во', u'от', u'со', u'для', u'о', u'же', u'ну', u'вы', u'бы', u'что', u'кто', u'он', u'она', u'который', u'этот',
+    u'сей', u'тот', u'также', u'быть', u'мочь', u'такой', u'кроме', u'-'])
+
+    text = f.read().decode('utf8')
+    text = re.sub(u'[^а-яА-Я\-\s]','',text) #delete all not bukva
+    articleText = articleText + " " + text.lower()
+
+    tokens = nltk.word_tokenize(articleText)
+    tokens = [i for i in tokens if (i not in stop_words)]
+
+    final = []
+
+    for a in tokens:
+        final.append(morph.parse(a)[0].normal_form)
+
+    return Counter(final)
 
 
-morph = pymorphy2.MorphAnalyzer()
 
-year = ['2011', '2012', '2013', '2014', '2015']
-month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-day = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15',
-       '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 
-text = ""
 
-for y in year:
-    for m in month:
-        for d in day:
+DIR = 'data'
+files = [f for f in listdir(DIR) if isfile(join(DIR, f))]
+
+for l in files:
+            l = l[0:-4]
+            dt = l.split("_")
             try:
-                f = open("data/" + y + "_" + m + "_"+d+".txt", 'rw')
-                text = text + f.read().decode('utf8').lower()
-
+                countArtOfDay(dt[0], dt[1], dt[2])
             except:
-                a=1
+                a = 1
 
-text=re.sub(u'[^А-Яа-яA-Za-z\s]*', u'', text)
+# tokens = [i for i in tokens if (i not in string.punctuation)]
+# tokens = [i for i in tokens if (i not in stop_words)]
+# final=[]
 
-tokens = nltk.word_tokenize(text)
-tokens = [i for i in tokens if ( i not in string.punctuation )]
-stop_words = stopwords.words('russian')
-stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', 'к', 'на','»','«','«За','а'])
-tokens = [i for i in tokens if ( i not in stop_words )]
+# for azaza in tokens:
+    # final.append(morph.parse(azaza)[0].normal_form)
 
+# c = Counter(final)
 
-final=[]
+# file = open('cleaned/final.txt', 'w')
 
-for azaza in tokens:
-    final.append(morph.parse(azaza)[0].normal_form)
+# for item in c:
+    # file.write(  (item +" : "+ str(c[item])+"\n").encode('utf8'))
 
-c = Counter(final)
-
-file = open('cleaned/final.txt', 'w')
-
-for item in c:
-    file.write(  (item +" : "+ str(c[item])+"\n").encode('utf8'))
-
-file.close()
+# file.close()
