f7690679ce587d8fe07e46c314101a571897e952
Рубайло Андрей < >
2016-05-24 16:38:08 +0500

    grand refactoring for python 3 ver.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from BeautifulSoup import BeautifulSoup
+import sys
+from bs4 import BeautifulSoup
 import urllib3
 import datetime # Новый модуль для работы с датой и временем
 
@@ -10,13 +11,10 @@ def makeUrl(year, month, day):
     return link
 
 
-
-
-
 def getArticleUrl(url):
     http = urllib3.PoolManager()
     r = http.request('GET', url)
-    soup = BeautifulSoup(''.join(r.data))
+    soup = BeautifulSoup(r.data,'html.parser')
     some = soup.find("ul", { "class" : "big_listing" })
     try:
         a = some.findAll("a", { "class" : "mkh2" })
@@ -29,46 +27,44 @@ def getArticleUrl(url):
 
 
 
-
-
-
 def getText(urlList):
-    http = urllib3.PoolManager()
-    cleanArts=""
-    for url in urlList:
-        r = http.request('GET', url)
-        soup = BeautifulSoup(''.join(r.data))
-        article = soup.find("div", { "class" : "content" })
-        for p in article.findAll('p'):
-            div = p.findAll("div")
-            [comment.replaceWith(" ") for comment in div]
-            a = p.findAll("a")
-            [comment.replaceWith(" ") for comment in a]
-            span = p.findAll("span")
-            [comment.replaceWith(" ") for comment in span]
-            cleanArts = cleanArts + " " + p.text
-        cleanArts=cleanArts+"\n\n"
-
-    return cleanArts
-
-
-
-
-
-dt = datetime.date(2015, 10, 10)
-timedelta1 = datetime.timedelta(days=23)
+	http = urllib3.PoolManager()
+	cleanArts = ""
+	for url in urlList:
+		r = http.request('GET', url)
+		soup = BeautifulSoup(r.data,'html.parser')
+		article = soup.find("div", { "class" : "content" })
+		if article is None:
+			continue
+		for p in article.findAll('p'):
+			div = p.findAll("div")
+			[comment.replaceWith(" ") for comment in div]
+			a = p.findAll("a")
+			[comment.replaceWith(" ") for comment in a]
+			span = p.findAll("span")
+			[comment.replaceWith(" ") for comment in span]
+			cleanArts = cleanArts + " " + p.text
+		cleanArts=cleanArts+"\n\n"
+	return cleanArts
 
 
 
-for x in xrange(100):
+
+dt = datetime.date(2014, 6, 23)
+timedelta = datetime.timedelta(days=1)
+
+
+
+
+for x in range(3600):
     url = makeUrl(str(dt.year), str(dt.month).zfill(2), str(dt.day).zfill(2))
     urlList = getArticleUrl(url)
     if urlList is None:
-        dt = dt - timedelta1
+        dt = dt + timedelta
         continue
     else:
         text = getText(urlList)
-        f = open('data/'+str(dt.year)+'_'+str(dt.month).zfill(2)+'_'+str(dt.day).zfill(2)+'.txt', 'w')
-        f.write(text.encode('utf8'))
+        f = open('data/'+str(dt.year)+'_'+str(dt.month).zfill(2)+'_'+str(dt.day).zfill(2)+'.txt', 'wb')
+        f.write(text.encode(sys.stdout.encoding, errors='replace'))
         f.close()
-        dt = dt - timedelta1
+        dt = dt + timedelta
--- cleaning.py
+++ cleaning.py
@@ -1,45 +1,54 @@
 # -*- coding: utf-8 -*-
 from os import listdir
 from os.path import isfile, join
-import nltk
+from nltk.tokenize import TweetTokenizer
 import pymorphy2
 import re
 import operator
 from stop_words import get_stop_words
 from collections import defaultdict
+import codecs
 
 Count = defaultdict(int)
 
 def countArtOfDay(y, m, d, Count):
-    try:
-        f = open("data/" + y + "_" + m + "_"+d+".txt", 'rw')
-    except:
-        raise
+	try:
+		f = codecs.open("data/" + y + "_" + m + "_"+d+".txt", 'r',encoding='utf8')
+		try:
+			f.read()
+			#print('utf8')
+			f = codecs.open("data/" + y + "_" + m + "_"+d+".txt", 'r',encoding='utf8')
+		except Exception:
+			#print('ascii')	
+			f = codecs.open("data/" + y + "_" + m + "_"+d+".txt", 'r')
+	except:
+		raise
 
-    morph = pymorphy2.MorphAnalyzer()
-    articleText = ""
-    stop_words = get_stop_words('russian')
-    # stop_words.extend([u'это', u'как', u'так', u'и', u'в', u'над', u'к', u'до', u'не', u'на', u'но', u'за', u'то', u'с', u'ли',
-    # u'а', u'во', u'от', u'со', u'для', u'о', u'же', u'ну', u'вы', u'бы', u'что', u'кто', u'он', u'она', u'который', u'этот',
-    # u'сей', u'тот', u'также', u'быть', u'мочь', u'такой', u'кроме', u'-', u'который',u'быть', u'мочь',
-    # u'стать', u'наш', u'свой',u'самый', u'другой',u'очень',u'дать', u'однако', u'весь', u'который', u'такой', u'самый', u'из-за', u'какой-то', u'который'])
+	morph = pymorphy2.MorphAnalyzer()
+	
+	articleText = ""
+	stop_words = get_stop_words('russian')
+	stop_words.extend([u'это', u'как', u'так', u'и', u'в', u'над', u'к', u'до', u'не', u'на', u'но', u'за', u'то', u'с', u'ли',
+	u'а', u'во', u'от', u'со', u'для', u'о', u'же', u'ну', u'вы', u'бы', u'что', u'кто', u'он', u'она', u'который', u'этот',
+	u'сей', u'тот', u'также', u'быть', u'мочь', u'такой', u'кроме', u'-', u'который',u'быть', u'мочь',
+	u'стать', u'наш', u'свой',u'самый', u'другой',u'очень',u'дать', u'однако', u'весь', u'который', u'такой', u'самый', u'из-за', u'какой-то', u'который'])
+	print(y,m,d)
+	text = f.read()
+	#print(text)
+	text = re.sub(u'[^а-яА-Я\-\s]','',text)
 
-    text = f.read().decode('utf8')
-    text = re.sub(u'[^а-яА-Я\-\s]','',text)
+	articleText = articleText + " " + text.lower()
+	tknzr = TweetTokenizer()
+	tokens = tknzr.tokenize(articleText)
+	tokens = [w for w in tokens if not w in stop_words]
+	final = []
 
-    articleText = articleText + " " + text.lower()
+	for a in tokens:
+		final.append(morph.parse(a)[0].normal_form)
 
-    tokens = nltk.word_tokenize(articleText)
-    tokens = [w for w in tokens if not w in stop_words]
-
-    final = []
-
-    for a in tokens:
-        final.append(morph.parse(a)[0].normal_form)
-
-    for word in final:
-        Count[word] += 1
-
+	for word in final:
+		Count[word] += 1
+	f.close()
 
 
 DIR = 'data' #path
@@ -49,15 +58,14 @@ for l in files:
     l = l[0:-4]
     dt = l.split("_")
     try:
+        #print(dt)
         articlesCount = countArtOfDay(dt[0], dt[1], dt[2], Count)
+        newlist = sorted(Count.items(), key=operator.itemgetter(1), reverse=True)
+        file = open('counts/'+dt[0]+'_'+dt[1]+'_'+dt[2]+'.txt', 'wb')
+        for bn in newlist:
+            file.write((bn[0]+" "+str(bn[1])+"\r\n").encode('utf8'))
+        file.close()
     except:
         a = 1
 
 
-newlist = sorted(Count.items(), key=operator.itemgetter(1), reverse=True)
-file = open('counts/counted.txt', 'w')
-
-for bn in newlist:
-    file.write((bn[0]+" "+str(bn[1])+"\n").encode('utf8'))
-
-file.close()
